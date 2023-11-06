2bcebb6163f55458d5bd58fd6305b2458f50a0ab
Josh Browning <joshua.browning@upstart.com>
2023-11-06 13:28:30 -0700

    update example

---
+++ example.py
@@ -0,0 +1,6 @@
+def get_action_zero(observation, lives, frame_number):
+    return 1
+
+
+def get_action_shoot(observation, lives, frame_number):
+    return 1
---
+++ grade_atari.py
@@ -0,0 +1,39 @@
+from logging import warning
+
+import gymnasium as gym
+
+from example import get_action_zero as get_action
+
+# env = gym.make("ALE/Galaxian-v5", render_mode="rgb_array")
+env = gym.make("ALE/Galaxian-v5", render_mode="human")
+
+
+observation, info = env.reset(seed=42)
+games_to_play = 5
+game_rewards = []
+for i in range(games_to_play):
+    terminated = False
+    truncated = False
+    total_reward = 0
+    while (not terminated) and (not truncated):
+        action = 0
+        try:
+            action = get_action(observation, info["lives"], info["frame_number"])
+        except:
+            warning(f"get_action() call failed, using 0 as action!")
+        if action not in env.action_space:
+            warning(f"Action {action} not in valid actions {env.action_space}, replacing with 0!")
+            action = 0
+
+        observation, reward, terminated, truncated, info = env.step(action)
+        total_reward += reward
+
+        env.render()
+
+    print(f"Game over, score was {total_reward}!")
+    game_rewards += [total_reward]
+    observation, info = env.reset()
+
+env.close()
+
+print(f"Rewards were {game_rewards}")
