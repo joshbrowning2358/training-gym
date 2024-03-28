c55bc5de9f0d04c3c08e072486f47cac1bc58cf6
Josh Browning <joshua.browning@upstart.com>
2024-03-27 20:49:08 -0600

    frozen lake intro

---
+++ intro_scripts/frozen_lake_n.py
@@ -0,0 +1,18 @@
+observation = {"position": 695, "board_size": 28}
+
+
+def get_coordinate(position, board_size):
+    row = 0
+    col = 0
+    return row, col
+
+
+position = 12
+board_size = 5
+# 0  1  2  3  4
+# 5  6  7  8  9
+# 10 11 12 13 14
+# Position 12 is row 2, col 2
+
+
+get_coordinate(12, 5)
