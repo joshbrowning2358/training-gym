f1e7ab3c68b9eeeb8ae124227dcf7a76d6fced39
Josh Browning <joshua.browning@upstart.com>
2024-09-13 16:36:21 -0600

    clean up farkle implementation

--- farkle/farkle.py
+++ farkle/farkle.py
@@ -47,61 +47,95 @@ class Farkle(Env):
             Tuple of observation, reward, terminated, truncated, info
         """
         if action["stop"] == 0:
-            # Continuing to roll, must assert that we kept dice worth something
-            kept_filter = [new == 1 and old == 0 for new, old in zip(action["keep"], self.current_keep)]
-            dice_to_score = np.array([die for die, keep in zip(self.dice, kept_filter) if keep])
-            score, used_all = self.score(dice_to_score)
-            if (score == 0) or (not used_all):
-                # Invalid move, tried to
-                #   a) keep playing without dice worth anything
-                #   b) didn't use all the dice they tried to keep
-                reward = 0
-                return self.end_turn(), reward, False, False, {}
-            else:
-                # Valid move, adding score to current score and rolling again
-                self.current_round_score += score
-                self.current_keep = [old or new for old, new in zip(self.current_keep, action["keep"])]
-                keep_arr = np.array(self.current_keep)
-                if all(keep_arr):
-                    # All dice have been kept, need to roll again
-                    self.current_keep = (0, 0, 0, 0, 0, 0)
-                    self.dice = np.random.randint(1, 7, 6)
-                else:
-                    # Reroll only non-kept dice
-                    self.dice[keep_arr == 0] = np.random.randint(1, 7, sum(1 - keep_arr))
-                return (
-                    {
-                        "current_score": self.current_score,
-                        "current_round_score": score,
-                        "current_keep": self.current_keep,
-                        "dice": self.dice,
-                    },
-                    score,
-                    False,
-                    False,
-                    {},
-                )
+            return self._move_keep_rolling(action)
         else:
-            # Ending turn and keeping current score
-            new_roll_filter = [keep == 0 for keep in self.current_keep]
-            dice_to_score = np.array([die for die, keep in zip(self.dice, new_roll_filter) if keep])
-            score, _ = self.score(dice_to_score)
-            self.current_round_score += score
-            if score == 0:
-                reward = 0
-                return self.end_turn(), reward, False, False, {}
-            else:
-                self.current_score += self.current_round_score
-                if self.current_score > WINNING_THRESHOLD:
-                    reward = 100
-                    return self.end_turn(), reward, True, False, {}
+            return self._move_stop_rolling()
 
-                # Scale the reward down a bit.  We want to reward the algo for scoring points, but
-                # not too much.  Really reward it once we win the game.
-                reward = self.current_round_score / 1000
-                self.current_round_score = 0
-                self.current_keep = (0, 0, 0, 0, 0, 0)
-                return self.end_turn(), reward, False, False, {}
+    def _move_keep_rolling(self, action: dict) -> tuple[dict, int | float, bool, bool, dict]:
+        """
+        Continuing to roll and keeping dice.  Must check that it's a valid set of dice to keep,
+        otherwise we need to return a reward of 0 and end the turn.
+
+        Args:
+            action: Dict with key "keep" specifying which dice should be kept.
+
+        Returns:
+            Tuple of observation, reward, terminated, truncated, info
+        """
+        # Continuing to roll, must assert that we kept dice worth something
+        kept_filter = [new == 1 and old == 0 for new, old in zip(action["keep"], self.current_keep)]
+        dice_to_score = np.array([die for die, keep in zip(self.dice, kept_filter) if keep])
+        score, used_all = self.score(dice_to_score)
+        if (score == 0) or (not used_all):
+            # Invalid move, tried to
+            #   a) keep playing without dice worth anything
+            #   b) didn't use all the dice they tried to keep
+            reward = 0
+            return self.end_turn(), reward, False, False, {}
+        else:
+            return self._valid_move_and_continuing(score, action)
+
+    def _move_stop_rolling(self) -> tuple[dict, int | float, bool, bool, dict]:
+        """
+        Ending turn and keeping current score.
+
+        Returns:
+            Tuple of observation, reward, terminated, truncated, info
+        """
+        new_roll_filter = [keep == 0 for keep in self.current_keep]
+        dice_to_score = np.array([die for die, keep in zip(self.dice, new_roll_filter) if keep])
+        score, _ = self.score(dice_to_score)
+        self.current_round_score += score
+        if score == 0:
+            reward = 0
+            return self.end_turn(), reward, False, False, {}
+        else:
+            self.current_score += self.current_round_score
+            if self.current_score > WINNING_THRESHOLD:
+                reward = 100
+                return self.end_turn(), reward, True, False, {}
+
+            # Scale the reward down a bit.  We want to reward the algo for scoring points, but
+            # not too much.  Really reward it once we win the game.
+            reward = self.current_round_score / 1000
+            self.current_round_score = 0
+            self.current_keep = (0, 0, 0, 0, 0, 0)
+            return self.end_turn(), reward, False, False, {}
+
+    def _valid_move_and_continuing(self, score: float, action: dict) -> tuple[dict, int | float, bool, bool, dict]:
+        """
+        Valid move, adding score to current score and rolling again.  Part of the move_keep_rolling
+        path.
+
+        Args:
+            score: The computed score of the kept dice.
+            action: Dict with key "keep" specifying which dice should be kept.
+
+        Returns:
+            Tuple of observation, reward, terminated, truncated, info
+        """
+        self.current_round_score += score
+        self.current_keep = [old or new for old, new in zip(self.current_keep, action["keep"])]
+        keep_arr = np.array(self.current_keep)
+        if all(keep_arr):
+            # All dice have been kept, need to roll again
+            self.current_keep = (0, 0, 0, 0, 0, 0)
+            self.dice = np.random.randint(1, 7, 6)
+        else:
+            # Reroll only non-kept dice
+            self.dice[keep_arr == 0] = np.random.randint(1, 7, sum(1 - keep_arr))
+        return (
+            {
+                "current_score": self.current_score,
+                "current_round_score": score,
+                "current_keep": self.current_keep,
+                "dice": self.dice,
+            },
+            score,
+            False,
+            False,
+            {},
+        )
 
     def end_turn(self) -> dict:
         """
